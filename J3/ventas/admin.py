4749ab3efe706cf51fa1c671b8a4e5e51586b75e
Jojans <johan.galvis0925@hotmail.com>
2025-01-17 19:16:20 -0500

    Database

--- J3/ventas/admin.py
+++ J3/ventas/admin.py
@@ -1,8 +1,1 @@
-from django.contrib import admin
-from .models import Product
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'category', 'rating', 'stock')  # Mostrar stock en la lista
-    search_fields = ('title', 'category')  # Buscar por título o categoría
-    list_filter = ('category',)  # Filtrar por categoría
--- J3/ventas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from ventas.models import Product
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['stock']  # Solo el campo stock
